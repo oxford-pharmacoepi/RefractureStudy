8e2f2c1a06ee434287d86fdaae5154c45b363b18
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-07-12 11:32:53 +0100

    update

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -14,10 +14,15 @@ info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 1 AND 2")
 source(here("2_CohortCreation", "CohortRQ1&2.R"))
 info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 1 AND 2 IS DONE")
 
-# carrying out analyses for Research Question 1 and 2
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1 AND 2")
-source(here("3_Analysis", "RQ1&2.R"))
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1 AND 2 IS DONE")
+# carrying out analyses for Research Question 1 
+info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1")
+source(here("3_Analysis", "RQ1Characterisation.R"))
+info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1 IS DONE")
+
+# carrying out analyses for Research Question 2
+info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 2")
+source(here("3_Analysis", "RQ2IncidenceCalculation.R"))
+info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 2 IS DONE")
 
 # generating cohorts for Research Question 3
 info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3")
@@ -26,5 +31,5 @@ info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3 IS DONE")
 
 # carrying out analyses for Research Question 3
 info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3")
-source(here("3_Analysis", "RQ3.R"))
+source(here("3_Analysis", "RQ3PropensityScores.R"))
 info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3 IS DONE")
