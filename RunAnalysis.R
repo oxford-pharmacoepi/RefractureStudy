4dfbe5523e5c7d03b924eb6fe5ebdabfc9ab0e58
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-07-14 17:38:12 +0100

    commit

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -14,22 +14,22 @@ info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 1 AND 2")
 source(here("2_CohortCreation", "CohortRQ1&2.R"))
 info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 1 AND 2 IS DONE")
 
-# carrying out analyses for Research Question 1 
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1")
-source(here("3_Analysis", "RQ1Characterisation.R"))
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1 IS DONE")
+# # carrying out analyses for Research Question 1 
+# info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1")
+# source(here("3_Analysis", "RQ1Characterisation.R"))
+# info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 1 IS DONE")
 
 # carrying out analyses for Research Question 2
 info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 2")
 source(here("3_Analysis", "RQ2IncidenceCalculation.R"))
 info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 2 IS DONE")
 
-# generating cohorts for Research Question 3
-info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3")
-source(here("2_CohortCreation", "CohortRQ3.R"))
-info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3 IS DONE")
+# # generating cohorts for Research Question 3
+# info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3")
+# source(here("2_CohortCreation", "CohortRQ3.R"))
+# info(logger, "GENERATING COHORTS FOR RESEARCH QUESTION 3 IS DONE")
 
-# carrying out analyses for Research Question 3
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3")
-source(here("3_Analysis", "RQ3PropensityScores.R"))
-info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3 IS DONE")
+# # carrying out analyses for Research Question 3
+# info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3")
+# source(here("3_Analysis", "RQ3PropensityScores.R"))
+# info(logger, "CARRYING OUT ANALYSES FOR RESEARCH QUESTION 3 IS DONE")
