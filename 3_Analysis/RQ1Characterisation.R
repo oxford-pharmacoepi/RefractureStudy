a4de15514d9bcbee28179e60675ded0acfd6878a
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-07-19 17:23:25 +0100

    v0

--- 3_Analysis/RQ1Characterisation.R
+++ 3_Analysis/RQ1Characterisation.R
@@ -1,1 +1,76 @@
-fracture_table_characterisation <- fracture_table
+# Creating follow up time
+info(logger, "CREATING FOLLOW UP TIME: FOLLOWUPEND")
+fracture_table_follow_up <- fracture_table
+
+# 730 days after the index date
+fracture_table_follow_up <- addInTwoYearsAfter(fracture_table_follow_up)
+
+# End of data collection (assuming each person has only one observation period)
+fracture_table_follow_up <- addInObsEndDate(fracture_table_follow_up)
+
+# Adding first cancer date after the index date
+fracture_table_follow_up <- addInCancerPostIndex(fracture_table_follow_up)
+
+# Adding first bone disease date after the index date
+fracture_table_follow_up <- addInBoneDiseasePostIndex(fracture_table_follow_up)
+
+# Add in first fracture date after the index dates
+fracture_table_follow_up <- addInNextFracture(fracture_table_follow_up)
+
+# Add in death date after the index date 
+fracture_table_follow_up <- addInDeath(fracture_table_follow_up)
+
+# Add in FOLLOWUPEND
+fracture_table_follow_up <- addInFollowUpEnd(fracture_table_follow_up)
+
+# Add in immFracture
+fracture_table_follow_up <- immFracture(fracture_table_follow_up)
+
+fracture_table_follow_up_back_up <- fracture_table_follow_up
+
+reverseEntryTable <- list()
+
+while (nrow(fracture_table_follow_up_back_up) > 0){
+  reverseEntryTable[[nrow(fracture_table_follow_up_back_up)]] <- fracture_table_follow_up_back_up %>% filter(follow_up_time > 0)
+  fracture_table_follow_up_back_up <- fracture_table_follow_up_back_up %>% filter(follow_up_time > 0)
+  fracture_table_follow_up_back_up <- nextFractureClean(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- noDeathOnIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- noCancerPriorOrOnIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- noBoneDiseasePriorOrOnIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInTwoYearsAfter(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInObsEndDate(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInCancerPostIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInBoneDiseasePostIndex(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInNextFracture(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInDeath(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- addInFollowUpEnd(fracture_table_follow_up_back_up)
+  fracture_table_follow_up_back_up <- fracture_table_follow_up_back_up %>% ungroup()
+  fracture_table_follow_up_back_up <- immFracture(fracture_table_follow_up_back_up)
+}
+
+reverseEntryTable <- reverseEntryTable[!sapply(reverseEntryTable, is.null)]
+
+#entryTable serves as a table recording people who entered the cohort at least once, twice etc..
+entryTable <- list()
+for (i in (1:length(reverseEntryTable))){
+  entryTable[[i]]<-reverseEntryTable[[length(reverseEntryTable)+1-i]]
+}
+
+stratifiedCohort <- list()
+for (i in (1:(length(entryTable)-1))){
+  stratifiedCohort[[i]] <- entryTable[[i]] %>% anti_join(entryTable[[i+1]], by = "subject_id")
+}
+
+stratifiedCohort[[length(entryTable)]] <- entryTable[[length(entryTable)]]
+
+### imminent fracture cohort
+imminentFractureCohort <- list()
+for (i in (1:length(stratifiedCohort))){
+  imminentFractureCohort[[i]] <- stratifiedCohort[[i]] %>% filter (imminentFracture==1) %>% select(subject_id)
+}
+
+imminentFractureCohortTotal <-data.frame()
+for (i in 1:length(imminentFractureCohort)){
+  imminentFractureCohortTotal<-rbind(imminentFractureCohortTotal, imminentFractureCohort[[i]])
+}
