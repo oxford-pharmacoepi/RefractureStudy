bb19aae4fd868e7b3edb8bb8048ed1a9bd06589e
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-07-25 17:13:22 +0100

    cohort creation done

--- 2_CohortCreation/CohortRQ3.R
+++ 2_CohortCreation/CohortRQ3.R
@@ -10,181 +10,6 @@ for (i in (1:numberPeriods)){
 }
 periodEnd[[numberPeriods+1]] <- study_end_date
 
-### Create a cohort of women only, age above 50 between 2010/04/01 and 2018/03/31
-info(logger, "CREATING DENOMINATOR - WOMEN WHO ARE ABOVE 50 WITHIN THE STUDY PERIOD")
-cdm <- generateDenominatorCohortSet(
-  cdm,
-  name = "denominator",
-  cohortDateRange = c(study_start_date, study_end_date),
-  ageGroup = list(c(50, 150)))
-
-### cohort with all records of fracture 
-info(logger, "COLLECTING ALL RECORDS OF FRACTURES FROM DENOMINATORS")
-cdm[["fracture"]] <- cdm[["denominator"]] %>% 
-  left_join(cdm[["condition_occurrence"]], by = c("subject_id" = "person_id")) %>%
-  filter(condition_concept_id %in% any_fracture_id) %>%
-  select(subject_id, cohort_start_date, cohort_end_date, condition_concept_id, condition_start_date) %>%
-  mutate(cohort_start_date = as.Date(as.character(cohort_start_date))) %>%
-  mutate(cohort_end_date = as.Date(as.character(cohort_end_date))) %>%
-  mutate(fracture_site = if (condition_concept_id %in% hip_fracture_id) {
-    "Hip"
-  }
-  else if (condition_concept_id %in% femur_fracture_id) {
-    "Femur"
-  }
-  else if (condition_concept_id %in% foot_fracture_id) {
-    "Foot"
-  }
-  else if (condition_concept_id %in% tib_fracture_id) {
-    "Tibia and Fibula"
-  }
-  else if (condition_concept_id %in% rib_fracture_id) {
-    "Rib"
-  }
-  else if (condition_concept_id %in% forearm_fracture_id) {
-    "Forearm"
-  }
-  else if (condition_concept_id %in% vert_fracture_id) {
-    "Vertebra"
-  }
-  else if (condition_concept_id %in% pelvic_fracture_id) {
-    "Pelvic"
-  }  
-  else if (condition_concept_id %in% humerus_fracture_id) {
-    "Humerus"
-  } 
-  else if (condition_concept_id %in% nonspecific_fracture_id) {
-    "Nonspecific"
-  }
-  )
-
-fracture_table <- cdm[["fracture"]] %>% collect()
-
-AttritionReportRQ3<-tibble(
-  cohort_definition_id = as.integer(1),
-  number_records = fracture_table %>% tally() %>% pull(),
-  number_subjects = fracture_table %>% distinct(subject_id) %>% tally() %>% pull(),
-  reason = "Starting Population"
-)
-
-### Removing fractures before the birth year
-info(logger, "REMOVING FRACTURES BEFORE THE BIRTH YEAR")
-fracture_table <- fracture_table %>%
-  left_join(cdm[["person"]], by = c("subject_id" = "person_id"), copy = T) %>%
-  mutate(fracture_year = as.numeric(format(condition_start_date, "%Y"))) %>%
-  filter(fracture_year >= year_of_birth) %>%
-  select(subject_id, cohort_start_date, cohort_end_date, condition_concept_id, condition_start_date, fracture_site)
-
-AttritionReportRQ3<- AttritionReportRQ3 %>% 
-  union_all(  
-    tibble(
-      cohort_definition_id = as.integer(1),
-      number_records = fracture_table %>% tally() %>% pull(),
-      number_subjects = fracture_table %>% distinct(subject_id) %>% tally() %>% pull(),
-      reason = "fracture happening before the birth year"
-    )
-  ) 
-
-### Removing the fractures that happen on the same day as a trauma and applying hierarchy 
-info(logger, "REMOVING FRACTURES THAT HAPPEN ON THE SAME DAY AS A TRAUMA")
-fracture_table <- fracture_table %>%
-  anti_join(cdm[["condition_occurrence"]] %>% filter(condition_concept_id %in% trauma_condition), by = c("subject_id" = "person_id", "condition_start_date"), copy = T)
-
-fracture_table <- fracture_table %>% 
-  anti_join(cdm[["observation"]] %>% filter(observation_concept_id %in% trauma_observation), by = c("subject_id" = "person_id", "condition_start_date" = "observation_date"), copy = T) 
-
-fracture_table<-fracture_table %>%
-  anti_join(
-    fracture_table %>% filter(!fracture_site=="Nonspecific") %>% group_by(subject_id, condition_start_date) %>% summarise(number_site = n_distinct(fracture_site)) %>% filter(number_site>=3),
-    by = c("subject_id", "condition_start_date")
-  )
-
-AttritionReportRQ3<- AttritionReportRQ3 %>% 
-  union_all(  
-    tibble(
-      cohort_definition_id = as.integer(1),
-      number_records = fracture_table %>% tally() %>% pull(),
-      number_subjects = fracture_table %>% distinct(subject_id) %>% tally() %>% pull(),
-      reason = "fracture happening on the same day as a trauma code"
-    )
-  ) 
-
-### Washout and removing re-recordings
-info(logger, "APPLYING WASHOUT PERIOD")
-sites <- c("Hip", "Femur", "Pelvic", "Vertebra", "Humerus", "Forearm", "Tibia and Fibula", "Rib", "Foot", "Nonspecific")
-
-fracture_table_back_up <- fracture_table
-index_fractures <- tibble()
-
-while(nrow(fracture_table_back_up)>0){
-  fracture_correction <- list()
-  for (i in (1:10)){
-    fracture_correction[[sites[i]]] <- fracture_table_back_up %>% 
-      group_by(subject_id) %>% 
-      filter(fracture_site == sites[[i]] | fracture_site == sites[[10]]) %>% 
-      summarise(min_date = min(condition_start_date, na.rm =T)) %>% 
-      mutate(site = sites[[i]])
-  }
-  
-  fracture_correction_nonspecific <- list()
-  
-  for (i in (1:10)){
-    fracture_correction_nonspecific <- rbind(fracture_correction_nonspecific, fracture_correction[[i]])
-  }
-  
-  fracture_table_back_up <- fracture_table_back_up %>% 
-    left_join(fracture_correction_nonspecific, by = c("subject_id", "fracture_site" = "site")) %>% # compute min date
-    mutate(gap_to_min_date = condition_start_date - min_date) %>%
-    filter(gap_to_min_date == 0 | gap_to_min_date > washout_period) %>%
-    group_by(subject_id, condition_start_date, fracture_site) %>%
-    arrange(condition_concept_id, .by_group = TRUE) %>% 
-    filter(row_number()==1) %>%
-    ungroup()
-  
-  fracture_table_back_up <- fracture_table_back_up %>%
-    left_join(fracture_table_back_up %>% group_by(subject_id) %>% filter (gap_to_min_date == 0) %>% count(), by = "subject_id") %>%
-    filter (!((n>1) & (gap_to_min_date==0) & (fracture_site=="Nonspecific")))
-  
-  index_fractures <- rbind(index_fractures, fracture_table_back_up %>% filter(gap_to_min_date==0) %>% select(-min_date, -gap_to_min_date, -n))
-  
-  fracture_table_back_up <- fracture_table_back_up %>% 
-    filter(!gap_to_min_date==0) %>%
-    select(-min_date, -gap_to_min_date, -n)
-}
-
-fracture_table <- index_fractures
-
-AttritionReportRQ3<- AttritionReportRQ3 %>% 
-  union_all(  
-    tibble(
-      cohort_definition_id = as.integer(1),
-      number_records = fracture_table %>% tally() %>% pull(),
-      number_subjects = fracture_table %>% distinct(subject_id) %>% tally() %>% pull(),
-      reason = "Clean out using washout period"
-    )
-  ) 
-
-# Applying Hierarchy to multiple records on the same day
-info(logger, "APPLYING HIERARCHY TO PREVENT MORE THAN ONE RECORD ON THE SAME DAY FOR THE SAME PERSON")
-
-fracture_table$fracture_site<-factor(fracture_table$fracture_site, levels = sites)
-
-fracture_table <- fracture_table %>%
-  group_by(subject_id, condition_start_date) %>%
-  arrange(fracture_site, .by_group = TRUE) %>% 
-  filter(row_number()==1) %>%
-  ungroup()
-
-AttritionReportRQ3<- AttritionReportRQ3 %>% 
-  union_all(  
-    tibble(
-      cohort_definition_id = as.integer(1),
-      number_records = fracture_table %>% tally() %>% pull(),
-      number_subjects = fracture_table %>% distinct(subject_id) %>% tally() %>% pull(),
-      reason = "Excluding records on the same day for the same person according to hierarchy"
-    )
-  )
-
 ### Creating relevant cohorts
 denom <- cdm[["denominator"]] %>% 
   mutate(cohort_start_date = as.Date(as.character(cohort_start_date))) %>%
@@ -265,3 +90,291 @@ for (i in (2:length(compCohort1))){
     anti_join(accumulativeTargetCohort[[i-1]], by = "subject_id")
 }
 
+AttritionReportRQ3<-tibble(
+  reason_id = 1,
+  cohort_definition = "Comparator Cohort 2",
+  number_records = compCohort2[[1]] %>% tally() %>% pull(),
+  number_subjects = compCohort2[[1]] %>% distinct(subject_id) %>% tally() %>% pull(),
+  reason = "Starting Population",
+  period = 1
+)
+
+for (i in (2:length(compCohort2))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 1,
+        cohort_definition = "Comparator Cohort 2",
+        number_records = compCohort2[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort2[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Starting Population",
+        period = i
+  )
+)
+}
+
+for (i in (1:length(compCohort1))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 1,
+        cohort_definition = "Comparator Cohort 1",
+        number_records = compCohort1[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort1[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Starting Population",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(targetCohort))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 1,
+        cohort_definition = "Target cohort",
+        number_records = targetCohort[[i]] %>% tally() %>% pull(),
+        number_subjects = targetCohort[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Starting Population",
+        period = i
+      )
+    )
+}
+
+### Exclusion based on death 
+for (i in (1:length(targetCohort))){
+  targetCohort[[i]] <- noDeathOnOrAfterIndex(targetCohort[[i]])
+}
+
+for (i in (1:length(compCohort1))){
+  compCohort1[[i]] <- noDeathOnOrAfterIndex(compCohort1[[i]])
+}
+
+for (i in (1:length(compCohort2))){
+  compCohort2[[i]] <- noDeathOnOrAfterIndex(compCohort2[[i]])
+}
+
+for (i in (1:length(targetCohort))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 2,
+        cohort_definition = "Target cohort",
+        number_records = targetCohort[[i]] %>% tally() %>% pull(),
+        number_subjects = targetCohort[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on death",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort1))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 2,
+        cohort_definition = "Comparator Cohort 1",
+        number_records = compCohort1[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort1[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on death",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort2))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 2,
+        cohort_definition = "Comparator Cohort 2",
+        number_records = compCohort2[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort2[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on death",
+        period = i
+      )
+    )
+}
+
+### Exclusion based on prior cancer 
+for (i in (1:length(targetCohort))){
+  targetCohort[[i]] <- noCancerPriorOrOnIndex(targetCohort[[i]])
+}
+
+for (i in (1:length(compCohort1))){
+  compCohort1[[i]] <- noCancerPriorOrOnIndex(compCohort1[[i]])
+}
+
+for (i in (1:length(compCohort2))){
+  compCohort2[[i]] <- noCancerPriorOrOnIndex(compCohort2[[i]])
+}
+
+for (i in (1:length(targetCohort))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 3,
+        cohort_definition = "Target cohort",
+        number_records = targetCohort[[i]] %>% tally() %>% pull(),
+        number_subjects = targetCohort[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on cancer dates before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort1))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 3,
+        cohort_definition = "Comparator Cohort 1",
+        number_records = compCohort1[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort1[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on cancer dates before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort2))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 3,
+        cohort_definition = "Comparator Cohort 2",
+        number_records = compCohort2[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort2[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on cancer dates before index date",
+        period = i
+      )
+    )
+}
+
+### Exclusion based on prior bone disease
+for (i in (1:length(targetCohort))){
+  targetCohort[[i]] <- noBoneDiseasePriorOrOnIndex(targetCohort[[i]])
+}
+
+for (i in (1:length(compCohort1))){
+  compCohort1[[i]] <- noBoneDiseasePriorOrOnIndex(compCohort1[[i]])
+}
+
+for (i in (1:length(compCohort2))){
+  compCohort2[[i]] <- noBoneDiseasePriorOrOnIndex(compCohort2[[i]])
+}
+
+for (i in (1:length(targetCohort))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 4,
+        cohort_definition = "Target cohort",
+        number_records = targetCohort[[i]] %>% tally() %>% pull(),
+        number_subjects = targetCohort[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on bone disease dates before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort1))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 4,
+        cohort_definition = "Comparator Cohort 1",
+        number_records = compCohort1[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort1[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on bone disease dates before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort2))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 4,
+        cohort_definition = "Comparator Cohort 2",
+        number_records = compCohort2[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort2[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based on bone disease dates before index date",
+        period = i
+      )
+    )
+}
+
+### Exclusion based on prior obs
+for (i in (1:length(targetCohort))){
+  targetCohort[[i]] <-targetCohort[[i]] %>% 
+    left_join(cdm[["observation_period"]], by = c("subject_id" = "person_id"), copy = T) %>% 
+    select(subject_id:fracture_site, observation_period_start_date, observation_period_end_date) %>%
+    mutate(days_prior_obs = index_date - observation_period_start_date, days_after_obs = observation_period_end_date - index_date) %>%
+    filter(days_prior_obs >= prior_observation, days_after_obs >= 0) %>%
+    select(subject_id, cohort_start_date, cohort_end_date, cohort_interval, period_start, period_end, condition_concept_id, index_date, fracture_site)
+  
+}
+
+for (i in (1:length(compCohort1))){
+  compCohort1[[i]] <-compCohort1[[i]] %>% 
+    left_join(cdm[["observation_period"]], by = c("subject_id" = "person_id"), copy = T) %>% 
+    select(subject_id:fracture_site, observation_period_start_date, observation_period_end_date) %>%
+    mutate(days_prior_obs = index_date - observation_period_start_date, days_after_obs = observation_period_end_date - index_date) %>%
+    filter(days_prior_obs >= prior_observation, days_after_obs >= 0) %>%
+    select(subject_id, cohort_start_date, cohort_end_date, cohort_interval, period_start, period_end, condition_concept_id, index_date, fracture_site)
+}
+
+for (i in (1:length(compCohort2))){
+  compCohort2[[i]] <-compCohort2[[i]] %>% 
+    left_join(cdm[["observation_period"]], by = c("subject_id" = "person_id"), copy = T) %>% 
+    select(subject_id:index_date, observation_period_start_date, observation_period_end_date) %>%
+    mutate(days_prior_obs = index_date - observation_period_start_date, days_after_obs = observation_period_end_date - index_date) %>%
+    filter(days_prior_obs >= prior_observation, days_after_obs >= 0) %>%
+    select(subject_id, cohort_start_date, cohort_end_date, cohort_interval, period_start, period_end, index_date)
+}
+
+for (i in (1:length(targetCohort))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 5,
+        cohort_definition = "Target cohort",
+        number_records = targetCohort[[i]] %>% tally() %>% pull(),
+        number_subjects = targetCohort[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based prior observation before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort1))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 5,
+        cohort_definition = "Comparator Cohort 1",
+        number_records = compCohort1[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort1[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based prior observation before index date",
+        period = i
+      )
+    )
+}
+
+for (i in (1:length(compCohort2))){
+  AttritionReportRQ3 <- AttritionReportRQ3 %>%
+    union_all(
+      tibble(
+        reason_id = 5,
+        cohort_definition = "Comparator Cohort 2",
+        number_records = compCohort2[[i]] %>% tally() %>% pull(),
+        number_subjects = compCohort2[[i]] %>% distinct(subject_id) %>% tally() %>% pull(),
+        reason = "Excluding based prior observation before index date",
+        period = i
+      )
+    )
+}
+
+write.xlsx(AttritionReportRQ3, file = here::here(output_folder, "AttritionReportRQ3.xlsx"))
