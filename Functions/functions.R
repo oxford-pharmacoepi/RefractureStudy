02d3667da8438a19d0b8e34590b8268f7aa22105
xihang-chen <114928037+xihang-chen@users.noreply.github.com>
2023-11-07 15:12:11 +0000

    Update functions.R

--- Functions/functions.R
+++ Functions/functions.R
@@ -311,3 +311,133 @@ reformat_table_one <- function(result_imm, result_no_imm, result_frac){
   )
   return(reformatted_table1)
 }
+
+# reformating of table ones
+reformat_table_one_iqvia <- function(result_imm, result_no_imm, result_frac){
+  
+  table_one_no_imm <- result_no_imm
+  table_one_imm <- result_imm
+  table_one_frac <- result_frac
+  reformatted_table1 <- data.frame(x = NA, y= NA, z = NA, w = NA)
+  n1<- table_one_no_imm %>% dplyr::filter(variable == "Number subjects") %>% dplyr::pull(estimate)
+  n2 <- table_one_imm %>% dplyr::filter(variable == "Number subjects") %>% dplyr::pull(estimate)
+  
+  # variables assembled by min/max etc
+  cat_var <- table_one_no_imm %>% dplyr::filter(estimate_type == "min") %>% dplyr::select(variable) %>% dplyr::distinct() %>% dplyr::pull(variable)
+  
+  for (i in (1:length(cat_var))){
+    reformatted_table1 <- rbind(reformatted_table1, 
+                                data.frame(x = paste0(cat_var[[i]], ", median (IQR)"), 
+                                           y = paste0(table_one_frac %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "median") %>% dplyr::pull(estimate),
+                                                      " (",
+                                                      table_one_frac %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q25") %>% dplyr::pull(estimate),
+                                                      "-",
+                                                      table_one_frac %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q75") %>% dplyr::pull(estimate),
+                                                      ")"),
+                                           z = paste0(table_one_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "median") %>% dplyr::pull(estimate),
+                                                      " (",
+                                                      table_one_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q25") %>% dplyr::pull(estimate),
+                                                      "-",
+                                                      table_one_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q75") %>% dplyr::pull(estimate),
+                                                      ")"),
+                                           w = paste0(table_one_no_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "median") %>% dplyr::pull(estimate),
+                                                      " (",
+                                                      table_one_no_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q25") %>% dplyr::pull(estimate),
+                                                      "-",
+                                                      table_one_no_imm %>% dplyr::filter(variable == cat_var[[i]]) %>% dplyr::filter(estimate_type == "q75") %>% dplyr::pull(estimate),
+                                                      ")"))
+    )
+  }
+  
+  # age group variables
+  age_var <- table_one_frac %>% 
+    dplyr::filter(variable == "Age group") %>% 
+    dplyr::select(variable_level) %>%
+    dplyr::distinct() %>%
+    dplyr::pull()
+  
+  for (i in (1:length(age_var))){
+    reformatted_table1 <- rbind(reformatted_table1, data.frame(x = paste0("Age Group, ", age_var[[i]], ", n(%)"),
+                                                               y = paste0(table_one_frac %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_frac %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               z = paste0(table_one_imm %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_imm %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               w = paste0(table_one_no_imm %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_no_imm %>% dplyr::filter(variable_level == age_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")")))
+  }
+  
+  
+  #condition variables
+  condition_var <- table_one_frac %>% 
+    dplyr::filter(variable == "Conditions flag -inf to 0 days") %>%
+    dplyr::filter(!variable_level == "Malignant neoplastic disease") %>%
+    dplyr::select(variable_level) %>%
+    dplyr::distinct() %>%
+    dplyr::pull(variable_level)
+  
+  for (i in (1:length(condition_var))){
+    reformatted_table1 <- rbind(reformatted_table1, data.frame(x = paste0(condition_var[[i]], ", n(%)"),
+                                                               y = paste0(table_one_frac %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_frac %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               z = paste0(table_one_imm %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_imm %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               w = paste0(table_one_no_imm %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_no_imm %>% dplyr::filter(variable_level == condition_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")")))
+  }
+  
+  #medication variables
+  medication_var <- table_one_frac %>% 
+    dplyr::filter(stringr::str_detect(variable, 'medications')) %>%
+    dplyr::filter(!variable == "Jb ox ucb s1 medications antineoplastic agents flag m365 to 0") %>%
+    dplyr::select(variable) %>%
+    dplyr::distinct() %>%
+    dplyr::pull(variable)
+  
+  for (i in (1:length(medication_var))){
+    reformatted_table1 <- rbind(reformatted_table1, data.frame(x = paste0(medication_var[[i]], ", n(%)"),
+                                                               y = paste0(table_one_frac %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_frac %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               z = paste0(table_one_imm %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_imm %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")"),
+                                                               w = paste0(table_one_no_imm %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "count") %>% dplyr::pull(estimate),
+                                                                          " (",
+                                                                          round(as.numeric(table_one_no_imm %>% dplyr::filter(variable == medication_var[[i]]) %>% dplyr::filter(estimate_type == "percentage") %>% dplyr::pull(estimate)), digits = 1),
+                                                                          ")")))
+  }
+  reformatted_table1 <- reformatted_table1 %>% dplyr::distinct()
+  ### adjusting colnames
+  reformatted_table1_meds <- reformatted_table1 %>%
+    dplyr::filter(stringr::str_detect(x, 'medications'))
+  reformatted_table1_meds[["x"]]<-substr(reformatted_table1_meds$x, 25, nchar(reformatted_table1_meds$x)-21)
+  reformatted_table1_meds <- reformatted_table1_meds %>% dplyr::mutate(x = paste0(x, ", n(%)"))
+  
+  reformatted_table1 <- rbind(reformatted_table1 %>% dplyr::filter(!stringr::str_detect(x, 'medications')), reformatted_table1_meds)
+  
+  # reformatted_table1 <- reformatted_table1 %>%
+  #   dplyr::mutate(y = ifelse((stringr::str_detect(z, '<5')|stringr::str_detect(w, '<5')), "obsecured", y))
+  
+  ###rename columns
+  colnames(reformatted_table1) <- c(
+    "Characteristic",
+    paste0("Overall (n = ", as.integer(n1)+as.integer(n2), ")"),
+    paste0("Imminent Fracture (n = ", n2, ")"),
+    paste0("No Imminent Fracture (n = ", n1, ")")
+  )
+  return(reformatted_table1)
+}
