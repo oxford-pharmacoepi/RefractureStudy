66ed7f440a61ec4aa3e40333a85b5e0886a96d0c
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-11-13 10:38:13 +0000

    v0

---
+++ Miscellaneous/SelectedCovariates.R
@@ -0,0 +1,55 @@
+load("~/RefractureStudy/Results_AURUM/30/tempData/selectedLassoFeatures01.RData")
+selected30 <- selectedLassoFeatures01
+load("~/RefractureStudy/Results_AURUM/90/tempData/selectedLassoFeatures01.RData")
+selected90 <- selectedLassoFeatures01
+
+selectedCovs30 <- list()
+selectedCovs90 <- list()
+
+for (i in (1:length(selected30))){
+  selectedCovs30[[i]] <- as.data.frame(selected30[[i]]) %>%
+    dplyr::mutate(period = i) %>%
+    dplyr::rename(selected_covariates = 'selected30[[i]]') %>%
+    dplyr::filter(!(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731"))))
+}
+selected30 <- Reduce(union_all, selectedCovs30)
+selected30[["selected_covariates"]] <- substr(selected30$selected_covariates, 2, nchar(selected30$selected_covariates)-2)
+write.xlsx(selected30, file = here::here(output_folder, "01selected30.xlsx"))
+
+for (i in (1:length(selected90))){
+  selectedCovs90[[i]] <- as.data.frame(selected90[[i]]) %>%
+    dplyr::mutate(period = i) %>%
+    dplyr::rename(selected_covariates = 'selected90[[i]]') %>%
+    dplyr::filter(!(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731"))))
+}
+selected90 <- Reduce(union_all, selectedCovs90)
+selected90[["selected_covariates"]] <- substr(selected90$selected_covariates, 2, nchar(selected90$selected_covariates)-2)
+write.xlsx(selected90, file = here::here(output_folder, "01selected90.xlsx"))
+
+load("~/RefractureStudy/Results_AURUM/30/tempData/selectedLassoFeatures12.RData")
+selected30 <- selectedLassoFeatures01
+load("~/RefractureStudy/Results_AURUM/90/tempData/selectedLassoFeatures12.RData")
+selected90 <- selectedLassoFeatures01
+
+selectedCovs30 <- list()
+selectedCovs90 <- list()
+
+for (i in (1:length(selected30))){
+  selectedCovs30[[i]] <- as.data.frame(selected30[[i]]) %>%
+    dplyr::mutate(period = i) %>%
+    dplyr::rename(selected_covariates = 'selected30[[i]]') %>%
+    dplyr::filter(!(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731"))))
+}
+selected30 <- Reduce(union_all, selectedCovs30)
+selected30[["selected_covariates"]] <- substr(selected30$selected_covariates, 2, nchar(selected30$selected_covariates)-2)
+write.xlsx(selected30, file = here::here(output_folder, "12selected30.xlsx"))
+
+for (i in (1:length(selected90))){
+  selectedCovs90[[i]] <- as.data.frame(selected90[[i]]) %>%
+    dplyr::mutate(period = i) %>%
+    dplyr::rename(selected_covariates = 'selected90[[i]]') %>%
+    dplyr::filter(!(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731"))))
+}
+selected90 <- Reduce(union_all, selectedCovs90)
+selected90[["selected_covariates"]] <- substr(selected90$selected_covariates, 2, nchar(selected90$selected_covariates)-2)
+write.xlsx(selected90, file = here::here(output_folder, "12selected90.xlsx"))
