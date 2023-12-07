17d5028b8f92d27c2eafd01c0f71fbe7230c5c78
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-12-07 17:10:38 +0000

    minor update

--- 4_HealthEconomics/Graphs_HE.R
+++ 4_HealthEconomics/Graphs_HE.R
@@ -58,24 +58,25 @@ visits_count_wide_temp <- filtered_visits_dt1 %>%
   pivot_wider(names_from = specialty, values_from = visit_count, values_fill = NA)
 
 # replace all NAs with 0
-visits_count_wide_temp_1 <- visits_count_wide_temp %>% mutate(across(everything(), .fns = ~replace_na(.,0)))
+visits_count_wide_temp_1 <- visits_count_wide_temp %>% 
+  dplyr::mutate(across(everything(), .fns = ~replace_na(.,0)))
 # count the visits
 # Columns to sum row-wise
 columns_to_sum <- colnames(visits_count_wide_temp_1)[12:77]  # Include all 66 column names here
 
 # Adding a new column with row-wise sum of selected columns
 visits_count_wide_temp_2 <- visits_count_wide_temp_1 %>%
-  mutate(total_visits_per_w  = rowSums(select(., all_of(columns_to_sum_pt1))),
+  dplyr::mutate(total_visits_per_w  = rowSums(select(., all_of(columns_to_sum_pt1))),
          cohort = "target")
 
 # Calculate the percentage of subjects for each total visit count
 target_total_visits_percentage <- visits_count_wide_temp_2 %>%
-  group_by(total_visits_per_w) %>%
-  summarize(Subject_Count = n(), .groups = 'drop') %>%
-  mutate(Percent_Subjects = Subject_Count / sum(Subject_Count) * 100)
+  dplyr::group_by(total_visits_per_w) %>%
+  dplyr::summarize(Subject_Count = n(), .groups = 'drop') %>%
+  dplyr::mutate(Percent_Subjects = Subject_Count / sum(Subject_Count) * 100)
 
 # Plotting
-(plot1 <- target_total_visits_percentage %>% 
+plot1 <- target_total_visits_percentage %>% 
     # filter(total_visits_per_w > 0) %>% 
     ggplot(aes(x = total_visits_per_w, y = Percent_Subjects)) +
 
@@ -87,7 +88,7 @@ target_total_visits_percentage <- visits_count_wide_temp_2 %>%
       plot.title = element_text(face = "bold", hjust = 0.5, size = 12),
       axis.title = element_text(hjust = 0.5, size = 10),
       panel.spacing = unit(0.1, "lines")
-    ))
+    )
 
   geom_bar(stat = "identity") +
   labs(x = "Total Number of Visits", y = "% of Subjects (entries)",
@@ -97,35 +98,35 @@ target_total_visits_percentage <- visits_count_wide_temp_2 %>%
     plot.title = element_text(face = "bold", hjust = 0.5, size = 12),
     axis.title = element_text(hjust = 0.5, size = 10),
     panel.spacing = unit(0.1, "lines")
-  ))
+  )
 
 # save the plot
 ggsave(here::here("4_Healtheconomics", "Results", "target_distribution_of_visits.PNG"))
 
 # create an interactive plot
-(plotly1 <- ggplotly(plot1))
+plotly1 <- ggplotly(plot1)
 
 # save interactive plot as a html file
 htmlwidgets::saveWidget(plotly::ggplotly(plot1), here::here("4_Healtheconomics", "Results", "target_distribution_of_visits_v2.html"))
 
 # Filter subjects with more than 100 visits
 target_subjects_over_100_visits <- visits_count_wide_temp_2 %>%
-  filter(total_visits_per_w > 100) %>%
-  select(subject_id) 
+  dplyr::filter(total_visits_per_w > 100) %>%
+  dplyr::select(subject_id) 
 
 # View the resulting data
 print(target_subjects_over_100_visits)
 
 # convert to long format
 target_visits_count_wide_to_long <- visits_count_wide_temp_2 %>% 
-  gather(specialty, visits, 12:(ncol(.))) %>% 
-  complete(subject_id, specialty, fill = list(visits = 0)) 
+  tidyr::gather(specialty, visits, 12:(ncol(.))) %>% 
+  tidyr::complete(subject_id, specialty, fill = list(visits = 0)) 
 
 # grouping by specialty to see which specilities had the majority of visits
 target_specialty_visits <- target_visits_count_wide_to_long %>% 
-  group_by(specialty) %>% 
-  summarise(Subject_Count_visits = sum(visits), .groups = 'drop') %>%
-  arrange(desc(Subject_Count_visits)) 
+  dplyr::group_by(specialty) %>% 
+  dplyr::summarise(Subject_Count_visits = sum(visits), .groups = 'drop') %>%
+  dplyr::arrange(desc(Subject_Count_visits)) 
 
 
 # Top 3 visited specialties: General Medical Practitioner,Community Practitioner, Health Care Support Worker
@@ -134,18 +135,18 @@ target_total_visits_sp <- target_visits_count_wide_to_long %>%
   # filter(specialty %in% c("General Medical Practitioner")) %>% 
 
  # filter(specialty %in% c("General Medical Practitioner")) %>% 
-  group_by(visits) %>% 
-  summarise(count_v = n()) %>% 
-  mutate(percent_count = count_v/sum(count_v) *100) %>% 
+  dplyr::group_by(visits) %>% 
+  dplyr::summarise(count_v = n()) %>% 
+  dplyr::mutate(percent_count = count_v/sum(count_v) *100) %>% 
   dplyr::ungroup()
 
-joined_df <- full_join(target_visits_count_wide_to_long, target_total_visits_sp,
+joined_df <- dplyr::full_join(target_visits_count_wide_to_long, target_total_visits_sp,
                        by =  "visits")
 # target_total_visits_top3 <- target_visits_count_wide_to_long %>% 
 #   filter(specialty %in% c("General Medical Practitioner",
 #                           "Community Practitioner", "Health Care Support Worker"))
 # Plotting -
-(plot2 <- joined_df %>% 
+plot2 <- joined_df %>% 
     # filter(visits >0) %>% 
     ggplot(aes(x = visits, y = percent_count)) +
     geom_bar(stat = "identity", position = position_dodge()) +
@@ -161,10 +162,6 @@ joined_df <- full_join(target_visits_count_wide_to_long, target_total_visits_sp,
       panel.spacing = unit(0.1, "lines"),
       strip.text.y = element_text(angle = 0)
     ) 
-
-)
- )
-
 
 # save the plot
 ggsave(here::here("4_Healtheconomics", "Results", "target_distribution_of_visits_top3.PNG"))
