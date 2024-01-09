32bd3f909aa51a770ddd9ad8334f9db56db26b3d
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-01-09 16:08:55 +0000

    update

--- Miscellaneous/SelectedCovariates.R
+++ Miscellaneous/SelectedCovariates.R
@@ -10,12 +10,31 @@ for (i in (1:length(selectedLassoFeatures01))){
 selected01 <- Reduce(union_all, selectedCovs01)
 selected01[["selected_covariates"]] <- substr(selected01$selected_covariates, 2, nchar(selected01$selected_covariates)-2)
 
+selected01 <- selected01 %>% 
+  dplyr::mutate(selected_covariates = as.integer(selected_covariates)) %>% 
+  dplyr::rename("concept_id" = "selected_covariates") %>% 
+  left_join(tbl(db, sql(paste0(
+    "SELECT * FROM ",
+    cdm_database_schema,
+    ".concept"
+  ))),
+  copy = T) %>%
+  dplyr::select(
+    "concept_id", "concept_name",
+    "domain_id", "vocabulary_id", "period"
+  ) %>%
+  dplyr::collect() %>% 
+  dplyr::rename("selected_covariates" = "concept_id")
+
 other_covs01 <- list()
 for (i in (1:length(selectedLassoFeatures01))){
   other_covs01[[i]] <- as.data.frame(selectedLassoFeatures01[[i]]) %>%
     dplyr::mutate(period = i) %>%
     dplyr::rename(selected_covariates = 'selectedLassoFeatures01[[i]]') %>%
-    dplyr::filter(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731", "prior_observation")))
+    dplyr::filter(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731", "prior_observation"))) %>% 
+    dplyr::mutate(concept_name = NA,
+                  domain_id = NA, 
+                  vocabulary_id = NA)
 }
 
 for (i in (1:length(selectedLassoFeatures01))){
@@ -38,12 +57,31 @@ for (i in (1:length(selectedLassoFeatures12))){
 selected12 <- Reduce(union_all, selectedCovs12)
 selected12[["selected_covariates"]] <- substr(selected12$selected_covariates, 2, nchar(selected12$selected_covariates)-2)
 
+selected12 <- selected12 %>% 
+dplyr::mutate(selected_covariates = as.integer(selected_covariates)) %>% 
+  dplyr::rename("concept_id" = "selected_covariates") %>% 
+  left_join(tbl(db, sql(paste0(
+    "SELECT * FROM ",
+    cdm_database_schema,
+    ".concept"
+  ))),
+  copy = T) %>%
+  dplyr::select(
+    "concept_id", "concept_name",
+    "domain_id", "vocabulary_id", "period"
+  ) %>%
+  dplyr::collect() %>% 
+  dplyr::rename("selected_covariates" = "concept_id")
+
 other_covs12 <- list()
 for (i in (1:length(selectedLassoFeatures12))){
   other_covs12[[i]] <- as.data.frame(selectedLassoFeatures12[[i]]) %>%
     dplyr::mutate(period = i) %>%
     dplyr::rename(selected_covariates = 'selectedLassoFeatures12[[i]]') %>%
-    dplyr::filter(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731", "prior_observation")))
+    dplyr::filter(selected_covariates %in%(c("age", "number_visits_m180_to_m1", "number_visits_m730_to_m181", "number_visits_minf_to_m731", "prior_observation"))) %>% 
+    dplyr::mutate(concept_name = NA,
+                  domain_id = NA, 
+                  vocabulary_id = NA)
 }
 
 for (i in (1:length(selectedLassoFeatures12))){
@@ -78,12 +116,3 @@ for (i in (1:length(match_results_01))){
 
 write.xlsx(coefficients_ps_01, file = here::here(sub_output_folder, "coefficients_ps_01.xlsx"))
 write.xlsx(coefficients_ps_12, file = here::here(sub_output_folder, "coefficients_ps_12.xlsx"))
-
-#################################
-selected01_names <- selected01 %>% 
-  dplyr::left_join(selected_covs %>% dplyr::mutate(Id = as.character(Id)), by = c("selected_covariates" = "Id"))
-write.xlsx(selected01_names, file = here::here(sub_output_folder, "selected01_names.xlsx"))
-
-selected12_names <- selected12 %>% 
-  dplyr::left_join(selected_covs %>% dplyr::mutate(Id = as.character(Id)), by = c("selected_covariates" = "Id"))
-write.xlsx(selected12_names, file = here::here(sub_output_folder, "selected12_names.xlsx"))
