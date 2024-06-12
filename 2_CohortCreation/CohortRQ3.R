5a35e79676d26603783d4d838e48110baa72a33a
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-06-12 17:48:49 +0100

    update to incorporate France and Italy

--- 2_CohortCreation/CohortRQ3.R
+++ 2_CohortCreation/CohortRQ3.R
@@ -1,4 +1,7 @@
 ### Creating periods
+if(country_setting %in% c("Italy", "France")){
+  study_start_date <- as.Date("2015-04-01")
+}
 print(paste0("Generating period-based denominators at ", Sys.time()))
 info(logger, "CREATING 6-MONTH PERIODS")
 periodStart <- c()
